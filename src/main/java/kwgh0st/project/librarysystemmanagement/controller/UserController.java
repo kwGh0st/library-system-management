2c15915bcacf8509cbd8595a0a822a7662b5b5ea
Karol Programuje <karol341654@gmail.com>
2024-01-09 12:29:15 +0100

    Adding User classes.

---
+++ src/main/java/kwgh0st/project/librarysystemmanagement/controller/UserController.java
@@ -0,0 +1,40 @@
+package kwgh0st.project.librarysystemmanagement.controller;
+
+import kwgh0st.project.librarysystemmanagement.model.User;
+import kwgh0st.project.librarysystemmanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
---
+++ src/main/java/kwgh0st/project/librarysystemmanagement/service/UserService.java
@@ -0,0 +1,34 @@
+package kwgh0st.project.librarysystemmanagement.service;
+
+import kwgh0st.project.librarysystemmanagement.model.User;
+import kwgh0st.project.librarysystemmanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
