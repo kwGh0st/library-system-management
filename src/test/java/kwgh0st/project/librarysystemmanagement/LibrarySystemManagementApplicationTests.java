b6de50e6197e458b20c8d8908263d5e521b21c99
Karol Programuje <karol341654@gmail.com>
2024-01-09 11:19:54 +0100

    Initial Commit.

---
+++ src/test/java/kwgh0st/project/librarysystemmanagement/LibrarySystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package kwgh0st.project.librarysystemmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
